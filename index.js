6a5f66369a337c5e6377d6a26d3f8d87665fba88
juliocesardemoraes <jcmcf2012@gmail.com>
2025-02-08 09:46:32 -0300

    feat: add a better version

--- index.js
+++ index.js
@@ -5,14 +5,30 @@ const handResultTag = document.getElementById("hand__result");
 const handResultPcTag = document.getElementById("hand__result__pc");
 
 const handChosenTag = document.getElementById("hand__choosen");
+const handChosenRefTag = document.getElementById("hand__choosen__ref");
 const handChosenPcTag = document.getElementById("hand__computer");
 
 const scoreTag = document.getElementById("score");
 const helpSectionTag = document.getElementById("help__section");
 
+const generatingResultTag = document.getElementById("generating__result");
+const h1ElementCountdownTag = generatingResultTag.querySelector("h1");
+const userScreenTag = document.getElementById("user__screen");
+const pcScreenTag = document.getElementById("pc__screen");
+
+const buttonsFunctionality = document.getElementById("buttons__selector");
+const buttons = buttonsFunctionality.querySelectorAll("div");
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+const disableEnableOptions = () => {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].classList.toggle("disabled");
+    buttons[i].disabled = true;
+  }
+};
 
 function escolher(valor) {
   const possibilidades = ["pedra", "papel", "tesoura"];
@@ -21,39 +37,62 @@ function escolher(valor) {
   handResultTag.style.display = "none";
   handResultPcTag.style.display = "none";
 
-  console.log(valor);
+  handChosenRefTag.src = `/public/${valor}.svg`;
 
-  handChosenTag.src = `/public/${valor}.svg`;
-  handChosenPcTag.src = `/public/${escolhaPC}.svg`;
+  generatingResultTag.classList.toggle("hidden");
+  userScreenTag.classList.toggle("hidden");
+  pcScreenTag.classList.toggle("hidden");
 
-  const ganhos = {
-    pedra: "tesoura",
-    papel: "pedra",
-    tesoura: "papel",
-  };
+  let counter = 3;
 
-  // se o valor for igual ao numeroGerado empate.
-  // se o valor for pedra - tesoura ganhou.
-  // se o valor não for esse - perdeu
+  disableEnableOptions();
 
-  handResultTag.style.display = "block";
-  handResultPcTag.style.display = "block";
+  const thisInterval = setInterval(() => {
+    counter--;
+    h1ElementCountdownTag.textContent = `${counter}`;
+    console.log("INTERVAL");
 
-  if (valor == escolhaPC) {
-    handResultTag.textContent = "Você empatou";
-    handResultPcTag.textContent = "Empate";
-    score;
-  } else if (ganhos[valor] == escolhaPC) {
-    handResultTag.textContent = "Você ganhou!";
-    handResultPcTag.textContent = "Computador perdeu";
-    score[0]++;
-  } else {
-    handResultTag.textContent = "Você perdeu";
-    handResultPcTag.textContent = "Computador ganhou";
-    score[1]++;
-  }
+    if (counter === 0) {
+      h1ElementCountdownTag.textContent = `${3}`;
 
-  scoreTag.textContent = `${score[0]}/${score[1]}`;
+      clearInterval(thisInterval);
+    }
+  }, 1000);
+
+  setTimeout(() => {
+    generatingResultTag.classList.toggle("hidden");
+    userScreenTag.classList.toggle("hidden");
+    pcScreenTag.classList.toggle("hidden");
+
+    handChosenTag.src = `/public/${valor}.svg`;
+    handChosenPcTag.src = `/public/${escolhaPC}.svg`;
+
+    const ganhos = {
+      pedra: "tesoura",
+      papel: "pedra",
+      tesoura: "papel",
+    };
+
+    handResultTag.style.display = "block";
+    handResultPcTag.style.display = "block";
+
+    if (valor == escolhaPC) {
+      handResultTag.textContent = "Você empatou";
+      handResultPcTag.textContent = "Empate";
+    } else if (ganhos[valor] == escolhaPC) {
+      handResultTag.textContent = "Você ganhou!";
+      handResultPcTag.textContent = "Computador perdeu";
+      score[0]++;
+    } else {
+      handResultTag.textContent = "Você perdeu";
+      handResultPcTag.textContent = "Computador ganhou";
+      score[1]++;
+    }
+
+    scoreTag.textContent = `${score[0]}/${score[1]}`;
+
+    disableEnableOptions();
+  }, 3000);
 }
 
 const showElement = (elementId) => {
